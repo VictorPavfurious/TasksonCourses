2005b1aaea113304a8463b29ccb467b14d131a9b
Victor <30474572+VictorPavfurious@users.noreply.github.com>
2018-08-09 13:22:22 +0300

    Update Task5.java

--- Substring/Task5.java
+++ Substring/Task5.java
@@ -21,3 +21,13 @@ public class Task5 {
         }
     }
 }
+/*
+Given an array of characters char[] length of N.
+        Please, reverse part of the array, from i to j indexes. Try to make it in-place (without additional memory).
+        0 <= i <= j < N.
+        Example
+        Input
+        2 4 - i, j indexes. abcdefg
+        Output
+        abedcfg - substring "cde" was reversed to "edc".
+*/
